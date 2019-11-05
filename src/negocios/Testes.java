5db9c5cb8b110c3f38f0a111bcdc0bea17c004de
Christian2019 <chrisschmidt2702@gmail.com>
2019-11-04 21:31:54 -0300

    Update Testes.java

--- src/negocios/Testes.java
+++ src/negocios/Testes.java
@@ -6,7 +6,7 @@ public class Testes {
 	public static Configuracao indentificacao(String a) {
 		if (a.equals("PC1")) {
 			main.apelidoMaquinaDestino = "PC2";
-			String ipDestino = "10.32.160.89";
+			String ipDestino = "10.32.143.19";
 			int porta = 9876;
 			String apelido = "PC1";
 			int tempoToken = 3;
@@ -17,7 +17,7 @@ public class Testes {
 		}
 		if (a.equals("PC2")) {
 			main.apelidoMaquinaDestino = "PC3";
-			String ipDestino = "10.32.162.61";
+			String ipDestino = "10.32.160.195";
 			int porta = 9876;
 			String apelido = "PC2";
 			int tempoToken = 3;
@@ -28,7 +28,7 @@ public class Testes {
 		}
 		if (a.equals("PC3")) {
 			main.apelidoMaquinaDestino = "PC1";
-			String ipDestino = "10.32.160.96";
+			String ipDestino = "10.32.143.21";
 			int porta = 9876;
 			String apelido = "PC3";
 			int tempoToken = 3;
